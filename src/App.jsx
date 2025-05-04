ea70987f08c03fa7b120fff288b31fda580b5d07
marcelomengech <marcelomengech@gmail.com>
2025-05-04 18:06:31 +0300

    Deleted tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,4 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar.jsx';
-// import Home from './pages/Home.jsx';
-// import LoginSignup from './pages/LoginSignup.jsx';
-// import Dashboard from './pages/Dashboard.jsx';
 
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/loginsignup" element={<LoginSignup />} />
-//         <Route path="/dashboard" element={<Dashboard />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 
 
 import React from 'react';
